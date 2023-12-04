4e6867e2d33f4513c0ca96ee2c38854edd2aebbb
Pip7777 <150479276+pip7777@users.noreply.github.com>
2023-12-04 18:00:17 +0100

    Add files via upload

--- mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
+++ mavenproject4/src/main/java/com/mycompany/mavenproject4/Mavenproject4.java
@@ -1,11 +1,14 @@
 package com.mycompany.mavenproject4;
 
+// GitHUB
+
 import com.mycompany.mavenproject4.exceptions.NonexistentEntityException;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.nio.file.Path;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -37,14 +40,65 @@ public class Mavenproject4 {
             // 1. Crear Estado
             Statement estado = conn.createStatement();
         
-            // 1. Creo el Controller
-            PersonasJpaController cont = new PersonasJpaController( emf );
+            // 1. Departamento
+            DepartamentoJpaController contDep = new DepartamentoJpaController( emf );
             
+            Departamento dpt = new Departamento(444,"Dpto44");
+            Departamento dpt2 = new Departamento(4555,"Dpto22");
+                       
+            System.out.println("DEP ID antes de crear :: " + dpt.getIdDepto() );
+            
+            contDep.create(dpt);
+            
+            System.out.println("DEP ID despues de crear " + dpt.getIdDepto() );
+            contDep.create(dpt2);
+            
+            dpt2.setIdDepto(1);            
+            dpt2.setNomDepto("AAAJAAAAK");
+            contDep.edit(dpt2);
+            
+            System.out.println("Recupero lista de Departamentos"); 
+            List<Departamento> list = contDep.findDepartamentoEntities();
+            
+            for( Departamento a :list )
+            {
+                System.out.println("Find == 1 " + a.getIdDepto() + "Dep: " + a.getNomDepto() );
+            }
+                        
+            // 2. Empleados
+            EmpleadoJpaController cont = new EmpleadoJpaController( emf );
+            Empleado emp1 = new Empleado("123456789H","Alpha Apellido1 Apellido2");
+            Empleado emp2 = new Empleado("987654321H","Beta Apellido1 Apellido2");
+            Empleado emp3 = new Empleado("333333333H","Gamma Apellido1 Apellido2");
+            cont.create(emp1);
+            cont.create(emp2);
+            cont.create(emp3);
+
+            emp1.setIdDepto(dpt);
+            emp2.setIdDepto(dpt);
+            cont.edit(emp1);
+            cont.edit(emp2);
+            
+            System.out.println("Datos de empleados"); 
+            
+            EmpleadoDatosProfJpaController contDatos = new EmpleadoDatosProfJpaController( emf );
+            EmpleadoDatosProf datos = new EmpleadoDatosProf();
+            
+            System.out.println("UPDATE DNI");
+            emp3.setDni("555555H");
+            System.out.println("EDIT ");
+            cont.edit(emp3);
+            System.out.println("END");
+                       
+            
+            /*
             Personas per = new Personas();
             per.setNombre("Maria");
             per.setId(1000);
             cont.edit(per);
+            */
             
+            /*
             // 2. Crear TABLA
             String crear = "CREATE TABLE IF NOT EXISTS Personas( id INTEGER PRIMARY KEY AUTO_INCREMENT, nombre VARCHAR(50) NOT NULL )";
             estado.execute( crear );            
@@ -92,6 +146,7 @@ public class Mavenproject4 {
             // 5. Cerrar Connexión
             estado.close();
             conn.close();
+            */            
         }        
         catch( SQLException ex )
         {
